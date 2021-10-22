8d39f4e32816f3ad3cc6c20fcc61896e1b289cba
Severin Reithmayer <reits2@bfh.ch>
2021-10-22 20:56:27 +0200

    Commit doch!

--- Illumination_Game/Capital.py
+++ Illumination_Game/Capital.py
@@ -1,15 +1,19 @@
-START_CAPITAL = 10000
-
 class Capital(object):
     """description of class"""
+
+    START_CAPITAL = 10000
 
     def __init__(self, init_amount = START_CAPITAL):
         self.currency_name = "Euro"
         self.currency_sign = "€"
         self.amount = init_amount
+        self.total_income_archive = []
+        self.total_cost_archive = []
 
     def subtract(self, number):
-        self.amount = self.amount - number
+        self.amount -= number
+        self.total_cost_archive.append(number)
 
     def add(self, number):
-        self.amount = self.amount + number
+        self.amount += number
+        self.total_income_archive.append(number)
--- Illumination_Game/Market.py
+++ Illumination_Game/Market.py
@@ -1,2 +1,23 @@
+from random import Random
+
 class Market(object):
     """description of class"""
+
+    def __init__(self, init_forecast = 0):
+        self.forecasted_sales = init_forecast
+        self.sales_forecast_archive = []
+        self.sales_modifer = 1
+        self.random_number_generater = Random()
+
+    def generate_sales_forecast(self, prod_id, min=1000, max=100000, step=100):
+        """prod_id can take values from 0-2. 0: light bulbs; 1: halogen lamp; 2: led light"""
+        self.forecasted_sales = self.random_number_generater.randrange(min, max, step)
+        self.sales_forecast_archive.append(self.forecasted_sales)
+        return self.forecasted_sales
+
+    def generate_sales_modifier(self, min=50, max=150, step=1):
+        self.sales_modifer = self.random_number_generater.randrange(min, max, step) / 100
+        return self.sales_modifer
+
+    def get_current_forecast(self):
+        return self.forecasted_sales
