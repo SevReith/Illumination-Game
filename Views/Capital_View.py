79e5e1b81b137e0fbb8197065cfd47f53fccbaf9
Severin Reithmayer <reits2@bfh.ch>
2021-12-05 10:46:05 +0100

    Aded cost detail update function.

--- Views/Capital_View.py
+++ Views/Capital_View.py
@@ -18,6 +18,7 @@ class Capital_View(QMdiArea):
         self._model_capital.latest_cost_changed.connect(self.update_lbl_cost)
         self._model_capital.latest_income_changed.connect(self.update_lbl_income)
         self._model_capital.latest_profit_changed.connect(self.update_lbl_profit)
+        self._model_capital.cost_detail_changed.connect(self.update_table_cost_detail)
     
 
     @pyqtSlot(float)
@@ -37,4 +38,15 @@ class Capital_View(QMdiArea):
         self._panel_accounting.tableWidget.setItem(row, 0, QTableWidgetItem(f'{cost:,.2f}'))
         self._panel_accounting.tableWidget.setItem(row, 1, QTableWidgetItem(f'{income:,.2f}'))
         self._panel_accounting.tableWidget.setItem(row, 2, QTableWidgetItem(f'{profit:,.2f}'))
+
+    @pyqtSlot(float, float, float, float)
+    def update_table_cost_detail(self, tot_cost, fixed_cost, mat_cost, build_cost):
+        """Take total cost, fixed cost, material cost and building cost and add them
+        to a new row in the cost detail table."""
+        row = self._panel_accounting.tableWidget_2.rowCount()
+        self._panel_accounting.tableWidget_2.insertRow(row)
+        self._panel_accounting.tableWidget.setItem(row, 0, QTableWidgetItem(f'{tot_cost:,.2f}'))
+        self._panel_accounting.tableWidget.setItem(row, 1, QTableWidgetItem(f'{fixed_cost:,.2f}'))
+        self._panel_accounting.tableWidget.setItem(row, 2, QTableWidgetItem(f'{mat_cost:,.2f}'))
+        self._panel_accounting.tableWidget.setItem(row, 3, QTableWidgetItem(f'{build_cost:,.2f}'))
      
